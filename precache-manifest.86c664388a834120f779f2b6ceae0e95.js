92f2671d4d3cbe0a33d82211825012207ae659a2
William Wong <williamwdev@gmail.com>
2020-07-30 16:02:14 -0700

    Updates

---
+++ precache-manifest.86c664388a834120f779f2b6ceae0e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802d60562ced069b2923aa5d3babf97c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f692583c861d74f57c",
+    "url": "/static/js/2.e62a55d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e62a55d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe755327700b0693161e",
+    "url": "/static/js/main.5c090534.chunk.js"
+  },
+  {
+    "revision": "d375bf9902ee1199c40f",
+    "url": "/static/js/runtime-main.630b3b29.js"
+  }
+]);
