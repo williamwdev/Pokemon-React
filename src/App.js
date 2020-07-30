be16d24b16eefb96ccccf6ce48fe43ca3debfc97
William Wong <williamwdev@gmail.com>
2020-07-30 14:48:17 -0700

    implemented axios to fetch from pokeAPI and rendering basic list on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then((res) => {
+      setPokemon(res.data.results.map((p) => p.name))
+    });
+  }, [currentPageUrl]);
+
   return (
     <div className="App">
-      Starting Point
+      <PokemonList pokemon={pokemon} />
     </div>
   );
 }
